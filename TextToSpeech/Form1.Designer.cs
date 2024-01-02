2e76650b6679158bd7a67b8523af2d49357c0655
zareen <zarru3131@gmail.com>
2024-01-02 17:57:27 +0530

    Add project files.

---
+++ TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtRecognizedSpeech = new System.Windows.Forms.TextBox();
+            this.btnStartRecognition = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(328, 48);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(656, 26);
+            this.txtText.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.SeaGreen;
+            this.btn1.Location = new System.Drawing.Point(594, 118);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(102, 50);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Run";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txtRecognizedSpeech
+            // 
+            this.txtRecognizedSpeech.Location = new System.Drawing.Point(404, 252);
+            this.txtRecognizedSpeech.Name = "txtRecognizedSpeech";
+            this.txtRecognizedSpeech.Size = new System.Drawing.Size(499, 26);
+            this.txtRecognizedSpeech.TabIndex = 2;
+            // 
+            // btnStartRecognition
+            // 
+            this.btnStartRecognition.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnStartRecognition.Location = new System.Drawing.Point(583, 305);
+            this.btnStartRecognition.Name = "btnStartRecognition";
+            this.btnStartRecognition.Size = new System.Drawing.Size(123, 87);
+            this.btnStartRecognition.TabIndex = 3;
+            this.btnStartRecognition.Text = "Start Recognition";
+            this.btnStartRecognition.UseVisualStyleBackColor = false;
+            this.btnStartRecognition.Click += new System.EventHandler(this.btnStartRecognition_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1297, 543);
+            this.Controls.Add(this.btnStartRecognition);
+            this.Controls.Add(this.txtRecognizedSpeech);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txtRecognizedSpeech;
+        private System.Windows.Forms.Button btnStartRecognition;
+    }
+}
+
---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        private SpeechRecognitionEngine recognitionEngine = new SpeechRecognitionEngine();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSpeechRecognition();
+        }
+
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string text = txtText.Text;
+            ReadText(text);
+        }
+
+        private void btnStartRecognition_Click(object sender, EventArgs e)
+        {
+            // Start listening for speech
+            try
+            {
+                recognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+                MessageBox.Show("Speech recognition started. Speak now.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error starting speech recognition: {ex.Message}");
+            }
+        }
+
+        private void readtext(string text)
+        {
+            try
+            {
+                synthesizer.Dispose();
+                synthesizer = new SpeechSynthesizer();
+                synthesizer.SpeakAsync(text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Speech Synthesis Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void InitializeSpeechRecognition()
+        {
+            // Load a dictation grammar to recognize any speech input
+            DictationGrammar dictationGrammar = new DictationGrammar();
+            recognitionEngine.LoadGrammar(dictationGrammar);
+
+            recognitionEngine.SpeechRecognized += RecognitionEngine_SpeechRecognized;
+
+            try
+            {
+                recognitionEngine.SetInputToDefaultAudioDevice();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing speech recognition: {ex.Message}");
+            }
+        }
+
+
+        private void RecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Display the recognized speech in the textbox
+            Invoke(new Action(() => txtRecognizedSpeech.Text = e.Result.Text));
+        }
+
+        private void ReadText(string text)
+        {
+            synthesizer.SpeakAsync(text);
+        }
+
+        private void btnReadText_Click(object sender, EventArgs e)
+        {
+            string text = txtText.Text;
+            ReadText(text);
+        }
+    }
+}
+
+
